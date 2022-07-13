359d8d5e344d08b4633edcf32ae89c52de5ac574
yashi jain <yashi.jain@comprotechnologies.com>
2022-07-13 12:44:40 +0530

    add url change event listener

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'url-change-event';
+
+function getNavigationUrl() {
+    window.addEventListener('urlchangeevent', function(e) {
+        console.log('from', e.oldURL.href);
+        console.log('to', e.newURL.href);
+    })
+}
+export default getNavigationUrl
