c0a42f3eda41a42c1152f717ec5cb28d1ab43508
yashi jain <yashi.jain@comprotechnologies.com>
2022-07-13 15:15:57 +0530

    add check for href  value

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import 'url-change-event';
 
 function getNavigationUrl() {
     window.addEventListener('urlchangeevent', function(e) {
-        console.log('from', e.oldURL.href);
-        if(e.newURL !== null) {
-            console.log('to', e.newURL.href);
-        }
-        
+        console.log('from', e.oldURL?.href);
+        console.log('to', e.newURL?.href);
     })
 }
 export default getNavigationUrl
