1038d787ba3ff9ae3f913000be9cea4e02b07c4d
yashi jain <yashi.jain@comprotechnologies.com>
2022-07-13 15:25:55 +0530

    remove check  value

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import 'url-change-event';
 
 function getNavigationUrl() {
     window.addEventListener('urlchangeevent', function(e) {
-        console.log('from', e.oldURL?.href);
-        console.log('to', e.newURL?.href);
+        console.log('from', e.oldURL.href);
+        console.log('to', e.newURL.href);
     })
 }
 export default getNavigationUrl
