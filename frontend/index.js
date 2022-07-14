fc21028eaec0be74f8359b316dc52417f3f27373
yashi jain <yashi.jain@comprotechnologies.com>
2022-07-14 12:54:00 +0530

    change in url

--- frontend/index.js
+++ frontend/index.js
@@ -30,7 +30,7 @@ function constructPageViewEvent(eventData) {
 }
 
 function sendToServer(event) {
-    const url = 'http//localhost:8080/apigateway/cav-analytics'
+    const url = 'http//localhost:5000/apigateway/cav-analytics'
     axios.post(url, event)
       .then((response)=>{
         console.log(response);
